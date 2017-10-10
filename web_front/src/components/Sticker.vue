113a917a8035b1a4a56bad4d9bf07ee7cb287f0a
carwest <carwestsam@gmail.com>
2017-10-11 01:29:34 +0800

    extract style to computed value

--- web_front/src/components/Sticker.vue
+++ web_front/src/components/Sticker.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="sticker">
+  <div
+    v-bind:style="styleObject" class="sticker">
     Work
   </div>
 </template>
-
+<script>
+export default {
+  name: 'sticker',
+  data () {
+    return {
+      styleProps: {
+        width: 100,
+        height: 100,
+        left: 100,
+        top: 100
+      }
+    }
+  },
+  computed: {
+    styleObject: function () {
+      return {
+        width: this.styleProps.width + 'px',
+        height: this.styleProps.width + 'px',
+        left: this.styleProps.width + 'px',
+        top: this.styleProps.width + 'px'
+      }
+    }
+  }
+}
+</script>
 <style lang="scss">
 .sticker{
   position: absolute;
-  left: 500px;
-  top: 100px;
   display: block;
   color: pink;
-  width: 100px;
-  height: 100px;
   background-color: yellow;
 }
 </style>
