c71c124c650d9328d5a050a651df6be36aa5a5e0
carwest <carwestsam@gmail.com>
2017-11-17 02:05:50 +0800

    first undoable mutation

---
+++ web_front/src/store/StateHistoryManager.js
@@ -0,0 +1,80 @@
+let shm
+export class StateHistoryMgr {
+  constructor () {
+    this.history = []
+    this.origin = {}
+    this.historyPt = 0 // next position
+  }
+  static getInstance () {
+    if (typeof shm === 'undefined') {
+      shm = new StateHistoryMgr()
+      window.shm = shm
+    }
+    return shm
+  }
+  addRawMutation (origin, key, raw) {
+    this.origin[key] = raw
+
+    origin.mutations[key] = function () {
+      let capsule = {}
+      shm.append(key, arguments, capsule)
+      raw().forward.apply(null, [capsule, ...arguments])
+    }
+  }
+
+  append (key, args, capsule) {
+    if (this.history.length > 0 && this.history.length > this.historyPt) {
+      this.history = this.history.slice(0, this.historyPt)
+    }
+    this.history.push({
+      mutation: key,
+      args,
+      capsule
+    })
+    this.historyPt += 1
+  }
+
+  emptyHistoryPointer () {
+    this.historyPt = 0
+    this.history = []
+    this.origin = {}
+  }
+
+  resetHistoryPointer () {
+  }
+
+  undo () {
+    if (this.historyPt === 0) {
+      return
+    }
+
+    let {mutation, args, capsule} = this.history[this.historyPt - 1]
+    this.origin[mutation]().backward.apply(null, [capsule, ...args])
+    this.historyPt -= 1
+  }
+
+  redo () {
+    if (this.historyPt === this.history.length) {
+      return
+    }
+    let {mutation, args, capsule} = this.history[this.historyPt]
+
+    this.origin[mutation]().forward.apply(null, [capsule, ...args])
+
+    this.historyPt += 1
+  }
+}
+shm = StateHistoryMgr.getInstance()
+export class VuexConfigGenerator {
+  // origin = {}
+  constructor (origin) {
+    this.origin = origin
+  }
+  attachMutations (attachObject) {
+    for (let key in attachObject) {
+      let shm = StateHistoryMgr.getInstance()
+      let func = attachObject[key]
+      shm.addRawMutation(this.origin, key, func)
+    }
+  }
+}
--- web_front/src/store/index.js
+++ web_front/src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import obj from '../utils/calcUtils.js'
 import _ from 'lodash'
 import uuid from 'uuid/v4'
+import {VuexConfigGenerator} from './StateHistoryManager'
 
 Vue.use(Vuex)
 
@@ -10,162 +11,176 @@ const INIT_ONBOARD_STICKER = 0
 const INIT_ONHAND_STICKER = 0
 const INIT_ONBOARD_GROUP = 0
 
-let getStore = function () {
-  return new Vuex.Store({
-    state: {
-      items: [],
-      onHand: []
-    },
-    mutations: {
-      randomStickers: (state, data) => {
-        let getRandomInt = obj.getRandomInt
-        state.stickers = []
-        for (let i = 0; i < INIT_ONBOARD_GROUP; i++) {
-          state.items.push({
-            id: uuid(),
-            type: 'group',
-            bg_color: '#' + getRandomInt(0, 0xffffff).toString(16),
-            text: 'group',
-            left: getRandomInt(0, 1000),
-            top: getRandomInt(0, 1000),
-            stack: 'board'
-          })
-        }
-        for (let i = 0; i < INIT_ONBOARD_STICKER; i++) {
-          state.items.push({
-            id: uuid(),
-            type: 'sticker',
-            bg_color: '#' + getRandomInt(0, 0xffffff).toString(16),
-            text: 'Hello World',
-            left: getRandomInt(0, 1000),
-            top: getRandomInt(0, 1000),
-            stack: 'board'
-          })
-        }
-        for (let i = 0; i < INIT_ONHAND_STICKER; i++) {
-          state.items.push({
-            id: uuid(),
-            type: 'sticker',
-            bg_color: '#' + getRandomInt(0, 0xffffff).toString(16),
-            text: 'on Hand',
-            stack: 'hand'
-          })
-        }
-      },
-      deleteItem: (state, {id}) => {
-        for (let i = 0; i < state.items.length; i++) {
-          if (state.items[i].id === id) {
-            state.items.splice(i, 1)
-            return
-          }
-        }
-      },
-      moveStickerToBoard: (state, {id, top, left}) => {
-        let newUUID = uuid()
-        for (let i = 0; i < state.items.length; i++) {
-          if (state.items[i].stack === id) {
-            state.items[i].stack = newUUID
-          }
-        }
-        for (let i = 0; i < state.items.length; i++) {
-          if (state.items[i].id === id) {
-            let sticker = _.cloneDeep(state.items[i])
-            sticker.left = left
-            sticker.top = top
-            sticker.id = newUUID
-            sticker.stack = 'board'
-            state.items.splice(i, 1)
-            state.items.push(sticker)
-            return
-          }
-        }
-      },
-      moveStickerToHand: (state, {id}) => {
-        let newUUID = uuid()
-        for (let i = 0; i < state.items.length; i++) {
-          if (state.items[i].stack === id) {
-            state.items[i].stack = newUUID
-          }
-        }
-        for (let i = 0; i < state.items.length; i++) {
-          if (state.items[i].id === id) {
-            let sticker = _.cloneDeep(state.items[i])
-            sticker.id = newUUID
-            sticker.stack = 'hand'
-            state.items.splice(i, 1)
-            state.items.push(sticker)
-            return
-          }
-        }
-      },
-      moveItem: (state, {id, itemId}) => {
-        if (id === itemId) {
-          return
-        }
-        let newUUID = uuid()
-        for (let i = 0; i < state.items.length; i++) {
-          if (state.items[i].stack === itemId) {
-            state.items[i].stack = newUUID
-          }
-        }
-        for (let i = 0; i < state.items.length; i++) {
-          if (state.items[i].id === itemId) {
-            let sticker = _.cloneDeep(state.items[i])
-            sticker.id = newUUID
-            sticker.stack = id
-            state.items.splice(i, 1)
-            state.items.push(sticker)
-            return
-          }
-        }
-      },
-      addStickerToHand: (state) => {
+let storeDes = {
+  state: {
+    items: [],
+    onHand: []
+  },
+  mutations: {
+    randomStickers: (state, data) => {
+      let getRandomInt = obj.getRandomInt
+      state.stickers = []
+      for (let i = 0; i < INIT_ONBOARD_GROUP; i++) {
+        state.items.push({
+          id: uuid(),
+          type: 'group',
+          bg_color: '#' + getRandomInt(0, 0xffffff).toString(16),
+          text: 'group',
+          left: getRandomInt(0, 1000),
+          top: getRandomInt(0, 1000),
+          stack: 'board'
+        })
+      }
+      for (let i = 0; i < INIT_ONBOARD_STICKER; i++) {
         state.items.push({
           id: uuid(),
           type: 'sticker',
-          bg_color: '#' + obj.getRandomInt(0, 0xffffff).toString(16),
-          text: 'New Sticker',
-          stack: 'hand'
+          bg_color: '#' + getRandomInt(0, 0xffffff).toString(16),
+          text: 'Hello World',
+          left: getRandomInt(0, 1000),
+          top: getRandomInt(0, 1000),
+          stack: 'board'
         })
-      },
-      addItem: (state, {type, stack}) => {
-        console.log('add item', type, stack)
+      }
+      for (let i = 0; i < INIT_ONHAND_STICKER; i++) {
         state.items.push({
           id: uuid(),
-          type,
-          bg_color: '#' + obj.getRandomInt(0, 0xffffff).toString(16),
-          text: 'New ' + type,
-          stack
+          type: 'sticker',
+          bg_color: '#' + getRandomInt(0, 0xffffff).toString(16),
+          text: 'on Hand',
+          stack: 'hand'
         })
-      },
-      updateStickerText: (state, {id, text}) => {
-        for (let i = 0; i < state.items.length; i++) {
-          if (state.items[i].id === id) {
-            state.items[i].text = text
-            break
-          }
+      }
+    },
+    moveStickerToBoard: (state, {id, top, left}) => {
+      let newUUID = uuid()
+      for (let i = 0; i < state.items.length; i++) {
+        if (state.items[i].stack === id) {
+          state.items[i].stack = newUUID
+        }
+      }
+      for (let i = 0; i < state.items.length; i++) {
+        if (state.items[i].id === id) {
+          let sticker = _.cloneDeep(state.items[i])
+          sticker.left = left
+          sticker.top = top
+          sticker.id = newUUID
+          sticker.stack = 'board'
+          state.items.splice(i, 1)
+          state.items.push(sticker)
+          return
         }
       }
     },
-    getters: {
-      stickers: (state) => {
-        return state.items.filter(sticker => sticker.stack === 'board')
-      },
-      onHand: (state) => {
-        return state.items.filter(sticker => sticker.stack === 'hand')
-      },
-      getStickerById: (state) => (id) => {
+    moveStickerToHand: (state, {id}) => {
+      let newUUID = uuid()
+      for (let i = 0; i < state.items.length; i++) {
+        if (state.items[i].stack === id) {
+          state.items[i].stack = newUUID
+        }
+      }
+      for (let i = 0; i < state.items.length; i++) {
+        if (state.items[i].id === id) {
+          let sticker = _.cloneDeep(state.items[i])
+          sticker.id = newUUID
+          sticker.stack = 'hand'
+          state.items.splice(i, 1)
+          state.items.push(sticker)
+          return
+        }
+      }
+    },
+    moveItem: (state, {id, itemId}) => {
+      if (id === itemId) {
+        return
+      }
+      let newUUID = uuid()
+      for (let i = 0; i < state.items.length; i++) {
+        if (state.items[i].stack === itemId) {
+          state.items[i].stack = newUUID
+        }
+      }
+      for (let i = 0; i < state.items.length; i++) {
+        if (state.items[i].id === itemId) {
+          let sticker = _.cloneDeep(state.items[i])
+          sticker.id = newUUID
+          sticker.stack = id
+          state.items.splice(i, 1)
+          state.items.push(sticker)
+          return
+        }
+      }
+    },
+    addStickerToHand: (state) => {
+      state.items.push({
+        id: uuid(),
+        type: 'sticker',
+        bg_color: '#' + obj.getRandomInt(0, 0xffffff).toString(16),
+        text: 'New Sticker',
+        stack: 'hand'
+      })
+    },
+    addItem: (state, {type, stack}) => {
+      console.log('add item', type, stack)
+      state.items.push({
+        id: uuid(),
+        type,
+        bg_color: '#' + obj.getRandomInt(0, 0xffffff).toString(16),
+        text: 'New ' + type,
+        stack
+      })
+    },
+    updateStickerText: (state, {id, text}) => {
+      for (let i = 0; i < state.items.length; i++) {
+        if (state.items[i].id === id) {
+          state.items[i].text = text
+          break
+        }
+      }
+    }
+  },
+  getters: {
+    stickers: (state) => {
+      return state.items.filter(sticker => sticker.stack === 'board')
+    },
+    onHand: (state) => {
+      return state.items.filter(sticker => sticker.stack === 'hand')
+    },
+    getStickerById: (state) => (id) => {
+      for (let i = 0; i < state.items.length; i++) {
+        if (state.items[i].id === id) {
+          return _.cloneDeep(state.items[i])
+        }
+      }
+    },
+    items: (state) => (stack = 'board') => {
+      return state.items.filter(item => item.stack === stack)
+    }
+  }
+}
+
+new VuexConfigGenerator(storeDes).attachMutations({
+  deleteItem: function () {
+    return {
+      forward: (capsule, state, {id}) => {
         for (let i = 0; i < state.items.length; i++) {
           if (state.items[i].id === id) {
-            return _.cloneDeep(state.items[i])
+            capsule['item'] = _.cloneDeep(state.items[i])
+            capsule['index'] = i
+            state.items.splice(i, 1)
+            return
           }
         }
       },
-      items: (state) => (stack = 'board') => {
-        return state.items.filter(item => item.stack === stack)
+      backward: (capsule, state, {id}) => {
+        state.items.splice(capsule['index'], 0, _.cloneDeep(capsule['item']))
       }
     }
-  })
+  }
+})
+
+let getStore = function () {
+  return new Vuex.Store(storeDes)
 }
 
 export default getStore
