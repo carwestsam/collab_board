7bbad3729904eadb48fb69d85fdaf97626e83dcb
carwest <carwestsam@gmail.com>
2018-01-26 02:57:44 +0800

    touch support for resizeable

--- web_front/src/directives/resizable.js
+++ web_front/src/directives/resizable.js
@@ -5,7 +5,8 @@ import _ from 'lodash'
 let cons = Vue.extend({
   template: `<div
     class="resizable-handle"
-    @mousedown.stop="onResize">
+    @mousedown.stop="onResize"
+    @touchstart.stop.prevent="onTouchResize">
     <v-icon>network_cell</v-icon>
   </div>`,
   methods: {
@@ -13,6 +14,7 @@ let cons = Vue.extend({
       let pe = this.$parentElement
       let pn = this.$parent
       let originDraggable = pe.getAttribute('draggable')
+      pe.setAttribute('draggable', 'false')
 
       let initWidth = parseInt(pn.context.styleProps.width)
       let initHeight = parseInt(pn.context.styleProps.height)
@@ -23,7 +25,6 @@ let cons = Vue.extend({
         return value / store.getters.scale
       }
 
-      pe.setAttribute('draggable', 'false')
       let onmousemove = function (ev) {
         let deltaX = scale(ev.clientX - initClientX)
         let deltaY = scale(ev.clientY - initClientY)
@@ -44,6 +45,40 @@ let cons = Vue.extend({
       }
       document.addEventListener('mousemove', onmousemove)
       document.addEventListener('mouseup', onmouseup)
+    },
+    onTouchResize: function (event) {
+      console.log('touchResize')
+      let pn = this.$parent
+      let initWidth = parseInt(pn.context.styleProps.width)
+      let initHeight = parseInt(pn.context.styleProps.height)
+      let initClientX = event.touches[0].clientX
+      let initClientY = event.touches[0].clientY
+      let deltaX = 0
+      let deltaY = 0
+
+      let scale = (value) => {
+        return value / store.getters.scale
+      }
+
+      let onmousemove = function (ev) {
+        deltaX = scale(ev.touches[0].clientX - initClientX)
+        deltaY = scale(ev.touches[0].clientY - initClientY)
+        if (_.hasIn(pn, 'context.onResizing')) {
+          pn.context.onResizing(initWidth + deltaX, initHeight + deltaY)
+        }
+      }
+      let onmouseup = function (ev) {
+        // let deltaX = scale(ev.touches[0].clientX - initClientX)
+        // let deltaY = scale(ev.toucheds[0].clientY - initClientY)
+        if (_.hasIn(pn, 'context.onResizeStop')) {
+          pn.context.onResizeStop(initWidth + deltaX, initHeight + deltaY)
+        }
+
+        document.removeEventListener('touchmove', onmousemove)
+        document.removeEventListener('touchend', onmouseup)
+      }
+      document.addEventListener('touchmove', onmousemove)
+      document.addEventListener('touchend', onmouseup)
     }
   }
 })
